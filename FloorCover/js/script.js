88d10fea4f6233dca0f3689945e0c0593c13856c
TinaClarinet <48104296+TinaClarinet@users.noreply.github.com>
2019-03-20 14:58:57 +0200

    Delete script.js

--- FloorCover/js/script.js
+++
@@ -1,6 +0,0 @@
-jQuery('document').ready(function () {
-    $(".nav-btn").on("click", function () {
-		var target = $(this).data("target");
-		$(target).toggleClass("nav__list--open") && $(".white").toggleClass("white-hidden");
-    });
-});
