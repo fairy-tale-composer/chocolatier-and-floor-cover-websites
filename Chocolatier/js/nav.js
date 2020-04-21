b797843fa398a580db8f3618ad5494db0553aa9d
TinaClarinet <valyachernova@mail.ru>
2020-04-21 15:31:32 +0300

    исправлены глюки

--- Chocolatier/js/nav.js
+++ Chocolatier/js/nav.js
@@ -1,17 +1,12 @@
 var toggler = document.querySelector('.nav-toggler'),
-	navWrap = document.querySelector('.top-fixed-wrap'),
-	navList = document.querySelector('.nav-list'),
-	navIcons = document.querySelector('.icons');
+	navWrap = document.querySelector('.top-fixed-wrap');
 
 toggler.onclick = function () {
-	navList.classList.toggle('nav-list-dropped');
 	navWrap.classList.toggle('nav-wrap-mob');
 	
-	if (navList.classList.contains('nav-list-dropped')) {
+	if (navWrap.classList.contains('nav-wrap-mob')) {
 		toggler.style.backgroundImage = 'url("img/menu-icon2.jpg")';
-		navIcons.style.visibility = 'visible';
 	} else {
 		toggler.style.backgroundImage = 'url("img/menu-icon1.jpg")';
-		navIcons.style.visibility = 'hidden';
 	}
 };
